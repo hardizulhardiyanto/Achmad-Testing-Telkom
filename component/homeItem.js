64bda0c3788bfcd8e886fddaa008fde9bd14fd9f
hardizulhardiyanto <azulhardiyanto@gmail.com>
2022-10-05 03:40:40 +0700

    HomeItem Added

--- component/homeItem.js
+++ component/homeItem.js
@@ -21,7 +21,6 @@ export default function HomeItem() {
             //willMount
         };
     }, [])
-    console.log('checKData >>', home)
     return (
         <div>
             <h1 className={styles.title}>
